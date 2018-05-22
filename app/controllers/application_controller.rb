db38342889ae07b46768b4672f6e42052f081ff9
David Zwagemaker <david@zwagemaker.nl>
2018-05-22 12:28:20 +0100

    Added new and create actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,22 @@ class ApplicationController < ActionController::Base
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
   private
   def fetch_task
     @task = Task.find(params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
