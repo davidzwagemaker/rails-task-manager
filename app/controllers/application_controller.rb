f838b5ec90458e13e51d8e40cdd1f89482835d48
David Zwagemaker <david@zwagemaker.nl>
2018-05-22 12:09:30 +0100

    Added index and show actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :fetch_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+  def fetch_task
+    @task = Task.find(params[:id])
+  end
 end
